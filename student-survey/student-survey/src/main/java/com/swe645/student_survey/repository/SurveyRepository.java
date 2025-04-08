89dbb3cc72a9b5c5b1a6742eb660304be9618073
Samruddhi0931 <dsamruddhi55@gmail.com>
2025-04-07 23:00:46 -0400

    Revert "added the springboot files"
    This reverts commit 55747bb8c690a8ffc0e9cdc76cc72bc98c4f90c4.

--- student-survey/student-survey/src/main/java/com/swe645/student_survey/repository/SurveyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.swe645.student_survey.repository;
-
-import com.swe645.student_survey.model.StudentSurvey;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SurveyRepository extends JpaRepository<StudentSurvey, Long> {
-}
