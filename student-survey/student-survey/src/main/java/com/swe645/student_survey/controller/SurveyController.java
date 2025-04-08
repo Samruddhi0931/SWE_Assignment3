55747bb8c690a8ffc0e9cdc76cc72bc98c4f90c4
Samruddhi0931 <dsamruddhi55@gmail.com>
2025-04-07 22:57:09 -0400

    added the springboot files

---
+++ student-survey/student-survey/src/main/java/com/swe645/student_survey/controller/SurveyController.java
@@ -0,0 +1,103 @@
+//package com.swe645.student_survey.controller;
+//
+//import com.swe645.student_survey.model.StudentSurvey;
+//import com.swe645.student_survey.repository.SurveyRepository;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.MediaType;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/api/surveys")
+//public class SurveyController {
+//
+//    @Autowired
+//    private SurveyRepository surveyRepository;
+//
+//    // 1️⃣ GET all surveys (JSON)
+//    @GetMapping
+//    public List<StudentSurvey> getAllSurveys() {
+//        return surveyRepository.findAll();
+//    }
+//
+//    // 2️⃣ POST survey from JSON (Postman or frontend)
+//    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+//    public StudentSurvey createSurveyJson(@RequestBody StudentSurvey survey) {
+//        return surveyRepository.save(survey);
+//    }
+//
+//    // 3️⃣ POST survey from HTML form (form-urlencoded)
+//    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+//    public StudentSurvey createSurveyForm(StudentSurvey survey) {
+//        return surveyRepository.save(survey);
+//    }
+//}
+// newcode
+package com.swe645.student_survey.controller;
+
+import com.swe645.student_survey.model.StudentSurvey;
+import com.swe645.student_survey.repository.SurveyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/surveys")
+public class SurveyController {
+
+    @Autowired
+    private SurveyRepository surveyRepository;
+
+    @PostMapping
+    public StudentSurvey createSurvey(@RequestBody StudentSurvey survey) {
+        return surveyRepository.save(survey);
+    }
+
+    @GetMapping
+    public List<StudentSurvey> getAllSurveys() {
+        return surveyRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StudentSurvey> getSurveyById(@PathVariable Long id) {
+        return surveyRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<StudentSurvey> updateSurvey(@PathVariable Long id, @RequestBody StudentSurvey updatedSurvey) {
+        return surveyRepository.findById(id)
+                .map(survey -> {
+                    survey.setFirstName(updatedSurvey.getFirstName());
+                    survey.setLastName(updatedSurvey.getLastName());
+                    survey.setStreetAddress(updatedSurvey.getStreetAddress());
+                    survey.setCity(updatedSurvey.getCity());
+                    survey.setState(updatedSurvey.getState());
+                    survey.setZip(updatedSurvey.getZip());
+                    survey.setTelephone(updatedSurvey.getTelephone());
+                    survey.setEmail(updatedSurvey.getEmail());
+                    survey.setSurveyDate(updatedSurvey.getSurveyDate());
+                    survey.setLikedMost(updatedSurvey.getLikedMost());
+                    survey.setInterestSource(updatedSurvey.getInterestSource());
+                    survey.setRecommendation(updatedSurvey.getRecommendation());
+                    survey.setRaffle(updatedSurvey.getRaffle());
+                    survey.setComments(updatedSurvey.getComments());
+
+                    return ResponseEntity.ok(surveyRepository.save(survey));
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSurvey(@PathVariable Long id) {
+        return surveyRepository.findById(id)
+                .map(survey -> {
+                    surveyRepository.delete(survey);
+                    return ResponseEntity.noContent().<Void>build();
+                }).orElse(ResponseEntity.notFound().build());
+    }
+}
